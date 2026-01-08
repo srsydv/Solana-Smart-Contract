b30dc2ab336d924a35ea6b043a6c214fb9f4c9df
srsydv <shrishofficial18@gmail.com>
2026-01-08 18:36:59 +0530

    Add Favorites account structure and SetFavorites instruction for managing user preferences

--- favorites/programs/favorites/src/lib.rs
+++ favorites/programs/favorites/src/lib.rs
@@ -14,3 +14,32 @@ pub mod favorites {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+// What we will put inside the Favorites PDA
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>
+}
+// When people call the set_favorites instruction, they will need to provide the accounts that will be modifed. This keeps Solana fast!
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed, 
+        payer = user, 
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, 
+        seeds=[b"favorites", user.key().as_ref()],
+    bump)]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
